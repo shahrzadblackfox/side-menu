11fee574d534b6e0cbb00dfe425915c13d19b7cb
Shahrzad <75183431+shahrzadblackfox@users.noreply.github.com>
2023-11-27 20:38:34 +0330

    adding context

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,11 @@
+import { useContext } from 'react';
 import React from 'react'
 import {FaBars} from 'react-icons/fa';
+import { AppContext } from './context';
 function Home() {
+
+     const data = useContext(AppContext);
+     
      return (
           <main>
                <button className="sidebar-toggle">
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { AppProvider } from './context';
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <AppProvider >
     <App />
+    </AppProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
